e513a0c50346c7d3355e2f7b86cc87651494a6de
Allison Basore <allison.basore@students.olin.edu>
2017-11-01 12:33:28 -0400

    added comments to start state

--- StartState.java
+++ StartState.java
@@ -7,6 +7,9 @@ import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 
+
+// A state for the user to see before starting the game
+
 public class StartState extends State{
 
 	public static BufferedImage img;
@@ -15,25 +18,20 @@ public class StartState extends State{
 	public int titleHeight;
 	public int titleWidth;
 	private World world;
-	//private GameState gameState;
 
 	public StartState(Handler handler){
-		super(handler); // getting it from the state class
+		super(handler);
 		world = new World(handler, "WorldsToUse/WorldD.txt");
-		//gameState = new GameState(handler);
+		// Using World D, other options are in the Folder WorldsToUse
 	}
 
 	public void tick(){
 		getInput();
-		
+		// Checks the input from the user for switching to the gameState
 	}
 
 	public void render(Graphics g){
-			
-		// g.setColor(Color.black);
-		// g.fillRect(10, 40, 20, 40); // x,y,width, height
-		// g.drawString("Ready to Play?", 50, 50);
-		// g.drawString("Press Down to Begin.", 100, 100);
+		// titleScreen1 is an image I made to direct the user to start the game
 		try {
 		    img = ImageIO.read(new File("titleScreen1.png"));
 		} catch (IOException e) {
@@ -44,23 +42,20 @@ public class StartState extends State{
 		int gameWidth = handler.getGame().getWidth();
 		int titleHeight = 180;
 		int titleWidth = 250;
-
+		
+		// Rendering the world in the background without any obstacles or helpers. This is just the tiles for visual affect
 		world.render(g);
+		
+		// Drawing the title screen and centering it
 		g.drawImage(img, (int) (gameWidth-titleWidth)/2, (int)(gameHeight-titleHeight)/2, titleWidth, titleHeight, null);
-		//g.drawImage(img, 100,100, 200, 200, null);
 	}
 
 	public void getInput(){
 		
 		if(handler.getKeyManager().aDown){
-			
+			// Checks if the user has hit the down arrow
 			State.setState(handler.getGame().getGameState());
 		}
 	}
-
- 	// public GameState getGameState(){
-	 // 	return gameState;
-	 // }
-
 
 }
