b13af3fa9e712e84408759aebe5f850373b6d7d8
Allison Basore <allison.basore@students.olin.edu>
2017-11-01 20:12:54 -0400

    added comments to water tile

--- WaterTile.java
+++ WaterTile.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.File;
 import java.awt.image.BufferedImage;
 
+// Class for displaying the water Tile
+// Image from https://commons.wikimedia.org/wiki/File:Water_texture_1380389_Nevit.jpg
 public class WaterTile extends Tile{
 
 	private BufferedImage img;
