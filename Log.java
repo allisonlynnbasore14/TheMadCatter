5a76f90ad85dbc98da3d33d499a788403f19c239
Allison Basore <allison.basore@students.olin.edu>
2017-11-02 09:07:00 -0400

    added comments to log

--- Log.java
+++ Log.java
@@ -7,10 +7,10 @@ import java.io.File;
 import java.io.IOException;
 
 public class Log extends Helpers{
-
-
+	// Log is the current only extension of Helpers
 	public static BufferedImage img;
-
+	// Image from http://www.clker.com/clipart-wood-sign-only.html
+	// Since the log is now taking up the whole water tile it is 40 by 20 instead of 20 by 20
 	public Log(Handler handler, float x, float y, String type){
 		super(handler, x, y, 20, 40, type);
 
