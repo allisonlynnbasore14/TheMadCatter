ba65b8b71627172bec3f0f48132517f91d21f361
Allison Basore <allison.basore@students.olin.edu>
2017-11-02 09:05:11 -0400

    added comments to obstacles

--- Obstacles.java
+++ Obstacles.java
@@ -3,12 +3,13 @@ import java.lang.*;
 import java.util.Random;
 
 public abstract class Obstacles extends Entity{
-
+	
 	public static final float DEFAULT_SPEED = 1.7f;
 	protected float xMove, yMove;
 	public float speed;
 	public float[] currentXpos;
 	private EndState endState;
+	// Making a randodm value
 	private Random rand = new Random();
 
 	public Obstacles(Handler handler, float x, float y, int width, int height, String type){
@@ -19,17 +20,20 @@ public abstract class Obstacles extends Entity{
 		endState = new EndState(handler);
 		
 	}
-
+	// 
 	public void move(){
 		moveX();
 		moveY();
-
 	}
 
 
 	public void moveX(){
 		int n = rand.nextInt(500) + 300; // 300 is the minimum and 500 is the max
-		int n2 = rand.nextInt(200) + 1; 
+		int n2 = rand.nextInt(200) + 1; // 1 is the minimum and 200 is the max (this number is multiplied by -1 below)
+		// The variables n and n2 are for the random placement of the obstalces off the screen
+		// This makes it so the obstacle appear at random times to the user
+		// n is for when the player is going right to left
+		// n2 is for when the obstacle is going left to right
 		boolean notCollided = !collisionDetection( x, y, type);
 		if(type == "Shark"){
 			if((int) x < 0 && notCollided){
@@ -53,11 +57,12 @@ public abstract class Obstacles extends Entity{
 		float ypos = handler.getGame().getGameState().getPlayer().getY();;
 		int playerSize = handler.getGame().getGameState().getPlayer().PLAYER_SIZE;
 		int obsSize = handler.getGame().getGameState().getPlayer().PLAYER_SIZE; 
-		// RIGHT NOW THE OBSTACLES ARE THE SAME SIZE AS THE PLAYERS
+		// Assuming the obstacle is the same size of the player
 		
 		boolean xConflict = (Math.abs(xpos + playerSize - (a + obsSize))<playerSize - 10);
 		boolean yConflict = (Math.abs(ypos + playerSize - (b + obsSize))<playerSize- 10);
-
+		// Checking if the player is conflicting with an obstacle and not on a log
+		// If the player is on a log it cannot be attacked by the shark obstacle
 		if(xConflict && yConflict && handler.getGame().getOnLog() == -1){
 			State.setState(endState);
 			return true;
@@ -67,11 +72,10 @@ public abstract class Obstacles extends Entity{
 	}
 
 	public void moveY(){
-		//boolean notCollided = !collisionDetection( x, y, type);
-		
-		//y += speed/10.0;
+		//The obstacles do not move in the y directionn
 	}
-
+	
+	// Getters and Setters
 
 	public float getSpeed(){
 		return speed;
