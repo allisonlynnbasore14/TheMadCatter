9544aab9e8bcd65477ad42fc834fee13a03a4a17
Allison Basore <allison.basore@students.olin.edu>
2017-11-02 08:10:26 -0400

    added comments to starter file

--- Starter.java
+++ Starter.java
@@ -2,7 +2,9 @@
 
 public class Starter{
 	public static void main( String[] args){
-		Game game = new Game("DONT FORGET THE GAME TITLE", 300, 300);
+		// This is the file that is first called
+		// It makes a game, title, size, and starts it
+		Game game = new Game("The Mad Catter", 300, 300);
 		game.start();
 	}
 }
